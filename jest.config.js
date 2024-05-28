0058d5dab3ba47738c31949089a859bf6dddb958
Jillian Vogel <jill@opencraft.com>
2024-05-28 11:33:09 +0930

    fix: add ts preset transforms to jest config
    as per https://github.com/openedx/frontend-build/blob/master/config/jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 const { createConfig } = require('@openedx/frontend-build');
+const { jsWithTs: tsjPreset } = require('ts-jest/presets');
 
 module.exports = createConfig('jest', {
   setupFilesAfterEnv: [
     '<rootDir>/src/setupTest.js',
   ],
+  transform: {
+    ...tsjPreset.transform,
+  },
 });
